dfb61a6f23dab43609332657bf2997f18ee3bc5f
강시우 <me@kangsiwoo.com>
2025-07-03 14:42:52 +0900

    feat: add ES token support

--- app/queues/task.py
+++ app/queues/task.py
@@ -1,8 +1,9 @@
 import asyncio
 
 class QueryTask:
-    def __init__(self, query: str):
-        self.query = query
+    def __init__(self, model: str, prompt: str):
+        self.model = model
+        self.prompt = prompt
         # 현재 이벤트 루프에 연결된 Future 생성
         loop = asyncio.get_event_loop()
         self.future = loop.create_future()
